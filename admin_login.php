7a673efd70c830520110db97df0be2cce33a90be
Mohammad Saniya <saniyamohammad0567@gmail.com>
2026-02-16 21:57:32 +0530

    updated

--- admin_login.php
+++ admin_login.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if ($username === 'admin' && $password === 'admin123') {
+    if ($username === 'user' && $password === 'userpass') {
         $_SESSION['admin_logged_in'] = true;
         header("Location: admin_dashboard.php");
         exit;
